b14951ada888e6ea152a5828dea3daba271a8e13
Trevor <me@trev.app>
2024-11-12 13:15:38 -0500

    Upgrade to .NET 6

--- QobuzApiSharp/Properties/AssemblyInfo.cs
+++ QobuzApiSharp/Properties/AssemblyInfo.cs
@@ -1,15 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("QobuzApiSharp")]
-[assembly: AssemblyDescription("C# library exposing public Qobuz v0.2 REST Api")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("DJDoubleD")]
-[assembly: AssemblyProduct("QobuzApiSharp")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("DJDoubleD")]
 [assembly: AssemblyCulture("")]
 
@@ -20,16 +10,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("66d7c0c6-272d-41d0-b12a-5578b44f4548")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.8.0")]
-[assembly: AssemblyFileVersion("0.0.8.0")]
